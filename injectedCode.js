d8e51a8c35773bea98161519fa79c81bbdbea555
Oleh Samorai <oleg.samorai@gmail.com>
2015-09-09 13:40:50 +0300

    fix

--- injectedCode.js
+++ injectedCode.js
@@ -1,43 +1,40 @@
 if (window.location.href.indexOf('/agar.io/') + 1) {
 
-    if (window.location.href.indexOf('/agar.io/') + 1) {
-        window.stop();
-        document.documentElement.innerHTML = 'Initialize plugin...';
+    window.stop();
+    document.documentElement.innerHTML = 'Initialize plugin...';
 
-        var xhr = new XMLHttpRequest();
+    var xhr = new XMLHttpRequest();
 
-        xhr.open('GET', window.location.href, true);
+    xhr.open('GET', window.location.href, true);
 
-        xhr.onerror = function () {
-            document.documentElement.innerHTML = 'Error getting Agar.io client';
-        };
+    xhr.onerror = function () {
+        document.documentElement.innerHTML = 'Error getting Agar.io client';
+    };
 
-        xhr.onload = function () {
-            var page = document.implementation.createHTMLDocument("");
-            page.documentElement.innerHTML = this.responseText;
+    xhr.onload = function () {
+        var page = document.implementation.createHTMLDocument("");
+        page.documentElement.innerHTML = this.responseText;
 
-            var newPage = document.importNode(page.documentElement, true);
+        var newPage = document.importNode(page.documentElement, true);
 
-            var nodeList = newPage.querySelectorAll('script');
-            for (var i = 0; i < nodeList.length; ++i) {
-                var node = nodeList[i];
-                if (node.innerText.indexOf('POSITIVE_INFINITY') > 0){
-                    node.removeAttribute('src');
-                    node.innerText = '';
-                }
-            }
-            document.replaceChild(newPage, document.documentElement);
-            var s;
-            var scripts = [
-                chrome.extension.getURL('client.js')
-            ];
-            for (i = 0; i < scripts.length; ++i) {
-                s = document.createElement('SCRIPT');
-                s.charset = 'UTF-8';
-                s.src = scripts[i];
-                document.getElementsByTagName('HEAD')[0].appendChild(s);
+        var nodeList = newPage.querySelectorAll('script');
+        for (var i = 0; i < nodeList.length; ++i) {
+            var node = nodeList[i];
+            if (node.innerText.indexOf('POSITIVE_INFINITY') > 0){
+                node.removeAttribute('src');
+                node.innerText = '';
             }
         }
-        xhr.send();
+        document.replaceChild(newPage, document.documentElement);
+        var s;
+        var scripts = [
+            chrome.extension.getURL('client.js')
+        ];
+        for (i = 0; i < scripts.length; ++i) {
+            s = document.createElement('SCRIPT');
+            s.charset = 'UTF-8';
+            s.src = scripts[i];
+            document.getElementsByTagName('HEAD')[0].appendChild(s);
+        }
     }
 }
